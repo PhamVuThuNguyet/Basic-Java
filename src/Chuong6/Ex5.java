06950139fcbcda7730c3acf910f2ffdabd536d69
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2021-11-30 16:16:09 +0700

    Chapter 6 updated

---
+++ src/Chuong6/Ex5.java
@@ -0,0 +1,83 @@
+package Chuong6;
+
+interface Vehicle{
+    // abstract method
+    public void speedUp(float v);
+    public void speedDown(float v);
+    public void break_();
+}
+
+class Motor implements Vehicle{
+    private float speed;
+
+    public Motor() {
+    }
+
+    public Motor(float speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public void speedUp(float v) {
+        this.speed += v;
+    }
+
+    @Override
+    public void speedDown(float v) {
+        this.speed -= v;
+    }
+
+    @Override
+    public void break_() {
+        this.speed = 0;
+    }
+
+    public String toString(){
+        return ("Speed: " + this.speed);
+    }
+
+}
+
+class Car implements Vehicle{
+    private float speed;
+
+    public Car() {
+    }
+
+    public Car(float speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public void speedUp(float v) {
+        this.speed *= v;
+    }
+
+    @Override
+    public void speedDown(float v) {
+        this.speed /= v;
+    }
+
+    @Override
+    public void break_() {
+        this.speed = 0;
+    }
+
+    public String toString(){
+        return ("Speed car: " + this.speed);
+    }
+}
+
+public class Ex5 {
+    public static void main(String[] args) {
+        Motor motor = new Motor(30);
+        motor.speedUp(15);
+        System.out.println(motor.toString());
+
+        Car car = new Car(10);
+        car.speedUp(2.5f);
+        System.out.println(car);
+    }
+}
+
+
